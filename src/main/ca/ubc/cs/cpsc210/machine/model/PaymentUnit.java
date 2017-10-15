7b89ab0922ceaec8d99537f8fa09fe1deda19b8e
HC <himanshuc71@gmail.com>
2017-10-14 22:37:45 -0700

    Assignment Done, attempt 1.

--- src/main/ca/ubc/cs/cpsc210/machine/model/PaymentUnit.java
+++ src/main/ca/ubc/cs/cpsc210/machine/model/PaymentUnit.java
@@ -1,5 +1,6 @@
 package ca.ubc.cs.cpsc210.machine.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,52 +16,103 @@ public class PaymentUnit {
     // EFFECTS: constructs a payment unit with no banked coins and no coins inserted into the machine
     // as part of a payment
     public PaymentUnit() {
-        //TODO: complete implementation
+        this.numLoonies = 0;
+        this.numQuarters = 0;
+        this.numDimes = 0;
+        this.numNickels = 0;
+        currentTransaction = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: clears all the coins banked in the unit
     public void clearCoinsBanked() {
-        //TODO: complete implementation
+        this.numLoonies = 0;
+        this.numQuarters = 0;
+        this.numDimes = 0;
+        this.numNickels = 0;
+
     }
 
     // REQUIRES: number > 0
     // MODIFIES: this
     // EFFECTS: adds number coins of type c to the banked coins in the unit
     public void addCoinsToBanked(Coin c, int number) {
-        //TODO: complete implementation
+        switch (c) {
+            case DIME:
+                this.numDimes = this.numDimes + number;
+                break;
+            case LOONIE:
+                this.numLoonies = this.numLoonies + number;
+                break;
+            case NICKEL:
+                this.numNickels = this.numNickels + number;
+                break;
+            case QUARTER:
+                this.numQuarters = this.numQuarters + number;
+                break;
+            default:
+                break;
+        }
     }
 
     // EFFECTS: returns number of coins banked of the given type
     public int getNumberOfCoinsBankedOfType(Coin c) {
-        //TODO: complete implementation
-        return 0;
+        int numCoins = 0;
+        switch (c) {
+            case DIME:
+                numCoins = this.numDimes;
+                break;
+            case LOONIE:
+                numCoins = this.numLoonies;
+                break;
+            case QUARTER:
+                numCoins = this.numQuarters;
+                break;
+            case NICKEL:
+                numCoins = this.numNickels;
+                break;
+            default:
+                break;
+        }
+        return numCoins;
     }
 
     // EFFECTS: returns the total value of all coins banked in the unit
     public int getValueOfCoinsBanked() {
-        //TODO: complete implementation
-        return 0;
+        return (this.numLoonies * Coin.LOONIE.getValue() + this.numQuarters * Coin.QUARTER.getValue() +
+                this.numDimes * Coin.DIME.getValue() + this.numNickels * Coin.NICKEL.getValue());
     }
 
     // MODIFIES: this
     // EFFECTS: adds coin c to the unit as a part of a transaction
     public void insertCoin(Coin c) {
-        //TODO: complete implementation
+        currentTransaction.add(c);
     }
 
     // EFFECTS: returns value of coins inserted for current transaction
     public int getValueOfCoinsInserted() {
-        //TODO: complete implementation
-        return 0;
+        int val = 0;
+        for (Coin next : currentTransaction) {
+            if (next == Coin.DIME)
+                val = val + Coin.DIME.getValue();
+            else if (next == Coin.LOONIE)
+                val = val + Coin.LOONIE.getValue();
+            else if (next == Coin.QUARTER)
+                val = val + Coin.QUARTER.getValue();
+            else
+                val = val + Coin.NICKEL.getValue();
+        }
+        return val;
     }
 
     // MODIFIES: this
     // EFFECTS: coins inserted for current transaction are cleared; list of coins
     // inserted for current transaction is returned in the order in which they were inserted.
     public List<Coin> cancelTransaction() {
-        //TODO: complete implementation
-        return null;
+        List<Coin> canceled = new ArrayList<>();
+        canceled.addAll(currentTransaction);
+        currentTransaction.clear();
+        return canceled;
     }
 
     // REQUIRES: cost <= total value of coins inserted as part of current transaction
@@ -69,7 +121,54 @@ public class PaymentUnit {
     // Coins of largest possible value are used when determining the change.  Change in full is not guaranteed -
     // will provide only as many coins as are banked in the machine, without going over the amount due.
     public List<Coin> makePurchase(int cost) {
-        //TODO: complete implementation
-        return null;
+        for (Coin next : currentTransaction) {
+            if (next == Coin.LOONIE)
+                this.numLoonies++;
+            else if (next == Coin.QUARTER)
+                this.numQuarters++;
+            else if (next == Coin.DIME)
+                this.numDimes++;
+            else
+                this.numNickels++;
+        }
+        return change(cost);
     }
+
+    //EFFECTS: returns the list of coins required as change for a transaction, helper for makePurchase()
+
+    public List<Coin> change(int cost) {
+        int l;
+        int q;
+        int d;
+        int n;
+        List<Coin> changeList = new ArrayList<>();
+        int change = getValueOfCoinsInserted() - cost;
+        if (change == 0)
+            return changeList;
+        else {
+            l = change / Coin.LOONIE.getValue();
+            change = change - l * Coin.LOONIE.getValue();
+            q = change / Coin.QUARTER.getValue();
+            change = change - q * Coin.QUARTER.getValue();
+            d = change / Coin.DIME.getValue();
+            change = change - d * Coin.QUARTER.getValue();
+            n = change / Coin.NICKEL.getValue();
+            addCoin(changeList, Coin.LOONIE, l);
+            addCoin(changeList, Coin.QUARTER, q);
+            addCoin(changeList, Coin.DIME, d);
+            addCoin(changeList, Coin.NICKEL, n);
+        }
+
+        return changeList;
+
+    }
+
+    //EFFECTS: adds a Coin to the List a certain number of times
+
+    public void addCoin(List<Coin> l, Coin c, int number) {
+        for (int i = 0; i < number; i++) {
+            l.add(c);
+        }
+    }
+
 }
--- src/test/ca/ubc/cs/cpsc210/machine/test/PaymentUnitTest.java
+++ src/test/ca/ubc/cs/cpsc210/machine/test/PaymentUnitTest.java
@@ -1,6 +1,145 @@
 package ca.ubc.cs.cpsc210.machine.test;
 
 
+import ca.ubc.cs.cpsc210.machine.model.Coin;
+import ca.ubc.cs.cpsc210.machine.model.PaymentUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PaymentUnitTest {
+
+    private PaymentUnit machine;
+
+    @BeforeEach
+    public void runBefore() {
+        machine = new PaymentUnit();
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(0, machine.getValueOfCoinsBanked());
+        assertEquals(0, machine.getValueOfCoinsInserted());
+
+    }
+
+    @Test
+    public void testAddCoinsToBanked() {
+        machine.addCoinsToBanked(Coin.LOONIE, 2);
+        assertEquals(200, machine.getValueOfCoinsBanked());
+        machine.addCoinsToBanked(Coin.QUARTER, 2);
+        assertEquals(250, machine.getValueOfCoinsBanked());
+        machine.addCoinsToBanked(Coin.DIME, 2);
+        assertEquals(270, machine.getValueOfCoinsBanked());
+        machine.addCoinsToBanked(Coin.NICKEL, 1);
+        assertEquals(275, machine.getValueOfCoinsBanked());
+    }
+
+    @Test
+    public void testClearCoinsBanked() {
+        assertEquals(0, machine.getValueOfCoinsBanked());
+        machine.clearCoinsBanked();
+        assertEquals(0, machine.getValueOfCoinsBanked());
+        machine.addCoinsToBanked(Coin.LOONIE, 1);
+        machine.addCoinsToBanked(Coin.NICKEL, 1);
+        assertEquals(105, machine.getValueOfCoinsBanked());
+        machine.clearCoinsBanked();
+        assertEquals(0, machine.getValueOfCoinsBanked());
+    }
+
+    @Test
+    public void testGetNumberOfCoinsBankedOfType() {
+        assertEquals(0, machine.getNumberOfCoinsBankedOfType(Coin.LOONIE));
+        assertEquals(0, machine.getNumberOfCoinsBankedOfType(Coin.NICKEL));
+        assertEquals(0, machine.getNumberOfCoinsBankedOfType(Coin.QUARTER));
+        assertEquals(0, machine.getNumberOfCoinsBankedOfType(Coin.DIME));
+
+        machine.addCoinsToBanked(Coin.LOONIE, 1);
+        machine.addCoinsToBanked(Coin.NICKEL, 1);
+        machine.addCoinsToBanked(Coin.QUARTER, 2);
+        machine.addCoinsToBanked(Coin.DIME, 2);
+
+        assertEquals(1, machine.getNumberOfCoinsBankedOfType(Coin.LOONIE));
+        assertEquals(1, machine.getNumberOfCoinsBankedOfType(Coin.NICKEL));
+        assertEquals(2, machine.getNumberOfCoinsBankedOfType(Coin.QUARTER));
+        assertEquals(2, machine.getNumberOfCoinsBankedOfType(Coin.DIME));
+
+    }
+
+    @Test
+
+    public void testInsertCoin() {
+        machine.insertCoin(Coin.LOONIE);
+        assertEquals(100, machine.getValueOfCoinsInserted());
+        machine.insertCoin(Coin.DIME);
+        machine.insertCoin(Coin.QUARTER);
+        machine.insertCoin(Coin.QUARTER);
+        machine.insertCoin(Coin.NICKEL);
+        assertEquals(165, machine.getValueOfCoinsInserted());
+
+    }
+
+    @Test
+
+    public void testCancelTransaction() {
+        List<Coin> cancelled;
+        cancelled = new ArrayList<>();
+        assertEquals(cancelled, machine.cancelTransaction());
+        machine.insertCoin(Coin.LOONIE);
+        machine.insertCoin(Coin.LOONIE);
+        machine.insertCoin(Coin.QUARTER);
+        cancelled.add(Coin.LOONIE);
+        cancelled.add(Coin.LOONIE);
+        cancelled.add(Coin.QUARTER);
+        assertEquals(cancelled, machine.cancelTransaction());
+
+    }
+
+    @Test
+
+    public void testMakePurchaseWhenNoChange() {
+        machine.insertCoin(Coin.LOONIE);
+        assertEquals(new ArrayList<Coin>(), machine.makePurchase(100));
+
+    }
+
+    @Test
+
+    public void testMakePurchaseWhenFullChangeCannotBeReturned() {
+        machine.addCoinsToBanked(Coin.QUARTER,3);
+        machine.addCoinsToBanked(Coin.DIME, 3);
+        machine.addCoinsToBanked(Coin.NICKEL, 2);
+        List<Coin> change;
+        machine.insertCoin(Coin.LOONIE);
+        change = new ArrayList<Coin>();
+        change.add(Coin.QUARTER);
+        change.add(Coin.QUARTER);
+        change.add(Coin.DIME);
+        assertEquals(change, machine.makePurchase(36));
+
+    }
+
+    @Test
+
+    public void testMakePurchaseWhenFullChangeCanBeReturned() {
+        machine.addCoinsToBanked(Coin.QUARTER,3);
+        machine.addCoinsToBanked(Coin.DIME, 3);
+        machine.addCoinsToBanked(Coin.NICKEL, 4);
+        List<Coin> change;
+        machine.insertCoin(Coin.LOONIE);
+        machine.insertCoin(Coin.QUARTER);
+        machine.insertCoin(Coin.QUARTER);
+        machine.insertCoin(Coin.DIME);
+        machine.insertCoin(Coin.NICKEL);
+        change = new ArrayList<Coin>();
+        change.add(Coin.DIME);
+        assertEquals(change, machine.makePurchase(155));
+
+    }
+
 
 }
